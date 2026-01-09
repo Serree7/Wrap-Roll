c817508d56a8bb886cd6f2ad7c54a95594e085d1
Ruben Serrejon <ruben.serrejonporras@riberadeltajo.es>
2026-01-09 19:15:20 +0100

    Cambios a las animaciones

--- js/script.js
+++ js/script.js
@@ -1,16 +1,46 @@
-// Get references to the burger button and nav menu
+// Selecciones del DOM
 const burgerButton = document.getElementById('menu-toggle');
 const navMenu = document.querySelector('.header__nav');
-const navLinks = navMenu.querySelectorAll('a'); // All links inside the nav menu
+const navLinks = navMenu.querySelectorAll('a');
 
-// Toggle mobile menu open/close on burger click
-burgerButton.addEventListener('click', () => {
-    navMenu.classList.toggle('nav--open'); 
-});
+// Mostrar menú
+const openMenu = () => {
+    navMenu.style.display = 'flex';      
+    navMenu.classList.remove('nav--close'); 
+    navMenu.classList.add('nav--open');   
+};
 
-// Close mobile menu when any link is clicked
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        navMenu.classList.remove('nav--open'); 
-    });
-});
+// Ocultar menú
+const closeMenu = () => {
+    navMenu.classList.remove('nav--open');  
+    navMenu.classList.add('nav--close');   
+
+    // Ocultamos al final de la animación
+    setTimeout(() => {
+        navMenu.classList.remove('nav--close');
+        navMenu.style.display = 'none';
+    }, 300); // Duración de la animación
+};
+
+// Alternar menú
+const toggleMenu = () => {
+    if (navMenu.classList.contains('nav--open')) {
+        closeMenu();
+    } else {
+        openMenu();
+    }
+};
+
+// Cerrar menú al hacer click en un enlace
+const handleLinkClick = () => {
+    if (navMenu.classList.contains('nav--open')) {
+        closeMenu();
+    }
+};
+
+
+// Listener del botón hamburguesa
+burgerButton.addEventListener('click', toggleMenu);
+
+// Listeners de los enlaces
+navLinks.forEach(link => link.addEventListener('click', handleLinkClick));
